a0cbf03c34f4622ccdfa9950562d7d3a52e69336
Arun Kannawadi <arunkannawadi@cmu.edu>
2017-06-11 21:34:14 +0200

    cosmetic modification

--- codes/code/python_codes/trial_codes/polar_shapelet_decomp.py
+++ codes/code/python_codes/trial_codes/polar_shapelet_decomp.py
@@ -8,15 +8,15 @@ from scipy import special
 Pi = math.pi
 factorial = math.factorial
 
-def Coeff(N, M, beta):
+def coeff(N, M, beta):
     B = (-1)**((N - np.abs(M))/2) / beta**(np.abs(M) + 1)
     C = (float(factorial(int((N-np.abs(M))/2)))/Pi/float(factorial((N+np.abs(M))/2)))**(.5)
     return B, C
 
 def polar_shapelets_real(N, M, beta):
 
-    coeff_1, coeff_2 = Coeff(N, M, beta)    
-    gen_laguerre = special.genlaguerre(n = (N-np.abs(M))/2, alpha = np.abs(M))
+    coeff_1, coeff_2 = coeff(N, M, beta)
+    gen_laguerre = special.genlaguerre(n=(N-np.abs(M))/2, alpha=np.abs(M))
 
     Laguer_N_M = lambda x, phi: coeff_1 * coeff_2 * x**(np.abs(M)) * gen_laguerre(x**2/beta**2) * np.exp(-x**2/2./beta**2) * np.exp(-1j*M*phi).real
     
@@ -24,7 +24,7 @@ def polar_shapelets_real(N, M, beta):
 
 def polar_shapelets_imag(N, M, beta):
     
-    coeff_1, coeff_2 = Coeff(N,M,beta)
+    coeff_1, coeff_2 = coeff(N,M,beta)
 
     gen_laguerre = special.genlaguerre(n = (N-np.abs(M))/2, alpha = np.abs(M))
 
@@ -38,13 +38,13 @@ def plot_shapelets(N,M, beta):
     Y = np.linspace(-5, 5, 100)
     Xv, Yv = meshgrid(X,Y)
     R = np.sqrt(Xv**2 +  Yv**2)
-    Phi = np.zeros_like(R)
+    phi = np.zeros_like(R)
     for i in xrange(np.shape(Xv)[0]):
         for j in xrange(np.shape(Xv)[1]):
-            Phi[i,j] = math.atan2(Yv[i,j], Xv[i,j])
+            phi[i,j] = math.atan2(Yv[i,j], Xv[i,j])
 
-    Func = polar_shapelets_imag(N, M, beta)(R, Phi)
-    im = imshow(Func, cmap = cm.bwr)
+    func = polar_shapelets_imag(N, M, beta)(R, phi)
+    im = imshow(func, cmap=cm.bwr)
     plt.show()
     
 if __name__ == "__main__":
