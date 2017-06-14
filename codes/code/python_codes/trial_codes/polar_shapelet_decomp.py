c1660ad0ca9664357ef393d1893d60710ed4e07a
Andrija Kostic <kole11koska@gmail.com>
2017-06-14 17:47:00 +0200

    Little bug fixed

--- codes/code/python_codes/trial_codes/polar_shapelet_decomp.py
+++ codes/code/python_codes/trial_codes/polar_shapelet_decomp.py
@@ -14,14 +14,31 @@ from scipy.integrate import quad,dblquad
 Pi = math.pi
 factorial = math.factorial
 
-def coeff(N, M, beta):
+def coeff_refregier(N, M, beta):
+    
+    """ Calculate coefficients for refregier
+    
+    @param N Energy quantum number
+    @param M Angular quantum number
+    @param beta Shapelet scale factor
+
+    @return B, C Normalization coefficients for shapelets
+    """
+
     B = (-1)**((N - np.abs(M))/2) / beta**(np.abs(M) + 1)
     C = (float(factorial(int((N-np.abs(M))/2)))/(Pi*float(factorial((N+np.abs(M))/2))))**(.5)
     return B, C
 
 def polar_shapelets_refregier(N, M, beta):
 
-    coeff_1, coeff_2 = coeff(N, M, beta)
+    """ Return callable function for polar shapelets expression from refregier
+
+    @param N, M Energy and momentum quantum numbers respectively
+    @param beta Shapelet scale
+
+    @return Callable function for shapelets in polar coordinates 
+    """
+    coeff_1, coeff_2 = coeff_refregier(N, M, beta)
     gen_laguerre = special.genlaguerre(n=(N-np.abs(M))/2, alpha=np.abs(M))
 
     laguer_N_M = lambda x, phi: coeff_1 * coeff_2 * x**(np.abs(M)) * gen_laguerre(x**2/beta**2) * np.exp(-x**2/2./beta**2) * np.exp(-1j*M*phi)
@@ -31,7 +48,7 @@ def polar_shapelets_refregier(N, M, beta):
 
 def polar_shapelets_real(N, M, beta):
 
-    coeff_1, coeff_2 = coeff(N, M, beta)
+    coeff_1, coeff_2 = coeff_refregier(N, M, beta)
     gen_laguerre = special.genlaguerre(n=(N-np.abs(M))/2, alpha=np.abs(M))
 
     laguer_N_M = lambda x, phi: coeff_1 * coeff_2 * x**(np.abs(M)) * gen_laguerre(x**2/beta**2) * np.exp(-x**2/2./beta**2) * np.exp(-1j*M*phi).real
@@ -40,7 +57,7 @@ def polar_shapelets_real(N, M, beta):
 
 def polar_shapelets_imag(N, M, beta):
     
-    coeff_1, coeff_2 = coeff(N,M,beta)
+    coeff_1, coeff_2 = coeff_refregier(N,M,beta)
 
     gen_laguerre = special.genlaguerre(n = (N-np.abs(M))/2, alpha = np.abs(M))
 
@@ -51,10 +68,21 @@ def polar_shapelets_imag(N, M, beta):
 #From Berry et al. Eq (12)
 
 def coeff_berry(N,M,beta):
+    
+    """ Calculate coefficients for expression from berry
+
+    @param N,M Energy and momentum quantum numbers respectively
+    @param beta Shapelet scale
+
+    @return Normalization coefficient as in berry
+    """
     return (2.*float(factorial((N-np.abs(M)/2)))/(Pi*beta**2*float(factorial((N+np.abs(M))/2))))**(0.5)
 
 def polar_shapelets_berry(N,M, beta):
 
+    """ Callable function 
+
+    """
     coeff = coeff_berry(N,M,beta)
     gen_laguerre = special.genlaguerre(n = (N-np.abs(M))/2, alpha = np.abs(M))
 
@@ -110,7 +138,7 @@ if __name__ == "__main__":
             print('berry')
             print(dblquad(lambda r,phi: r*polar_shapelets_berry(n,m,beta)(r,phi).conjugate()*polar_shapelets_berry(n,m,beta)(r,phi), 0, np.inf, lambda r: 0, lambda r: 2*Pi))
             print('refregier')
-            print(dblquad(lambda r,phi: r*polar_shapelets_regregier(n,m,beta)(r,phi).conjugate()*polar_shapelets_refregier(n,m,beta)(r,phi), 0, np.inf, lambda r: 0, lambda r: 2*Pi))    
+            print(dblquad(lambda r,phi: r*polar_shapelets_refregier(n,m,beta)(r,phi).conjugate()*polar_shapelets_refregier(n,m,beta)(r,phi), 0, np.inf, lambda r: 0, lambda r: 2*Pi))    
     #check_orthonormality() 
     #print(polar_shapelets(1,1,0,Pi/2, 1.))     
    
