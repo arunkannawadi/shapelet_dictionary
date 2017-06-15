Angular - real / Correct normalization (?) / SVD still has high order coefs

codes/code/python_codes/shapelet_dicts.py
@@ -10,7 +10,7 @@ import pyfits
 import galsim
 import math
 
-#import pdb; pdb.set_trace()
+import pdb; pdb.set_trace()
 
 #------------------
 # berry == Berry et al. MNRAS 2004
@@ -328,12 +328,13 @@ def shapelet_decomposition(N1=20,N2=20, basis = 'XY', solver = 'sparse', noise =
     cubr_real_noiseless = pyfits.getdata('../../data/cube_real_noiseless.fits')
     background = 1.e6*0.16**2
     img = galsim.Image(78,78) # cube_real has 100, 78x78 images
+    
     if (basis == 'XY') or (basis == 'Elliptical'):
         D = np.zeros((78*78,4*N1*N2)) # alloc for Dictionary
         base_coefs = np.zeros((N1,N2))
     elif (basis == 'Polar'):
-        D = np.zeros((78*78,4*N1*N2), dtype=complex)
-        base_coefs = np.zeros((N1,N2), dtype=complex)
+        D = np.zeros((78*78,4*N1*N2))#, dtype=complex)
+        base_coefs = np.zeros((N1,N2))#, dtype=complex)
 
     X = np.linspace(0,77,78)  
     Y = np.linspace(0,77,78)
@@ -344,7 +345,7 @@ def shapelet_decomposition(N1=20,N2=20, basis = 'XY', solver = 'sparse', noise =
         shape = img.FindAdaptiveMom()
         x0,y0 = shape.moments_centroid.x, shape.moments_centroid.y ## possible swap b/w x,y
         sigma = shape.moments_sigma
-       
+        n_max = 20
 
         #Make a meshgrid for the polar shapelets
 
@@ -357,7 +358,6 @@ def shapelet_decomposition(N1=20,N2=20, basis = 'XY', solver = 'sparse', noise =
                 Phi[i,j] = math.atan2(Yv[i,j], Xv[i,j])
 
         signal = cube_real[img_idx].flatten()
-
         if (noise == 1):
             import random
             random.seed()
@@ -375,7 +375,7 @@ def shapelet_decomposition(N1=20,N2=20, basis = 'XY', solver = 'sparse', noise =
             #base_coefs_im = np.zeros_like(base_coefs)
             for n in xrange(N1):
                 for m in xrange(-n,n+1,2):
-                    if (n <= (78/sigma - 1)): # n_max ~ theta_max (image size) / theta_min (pixel or kernel smoothing size) -1 
+                    if (n <= n_max): # n_max ~ theta_max (image size) / theta_min (pixel or kernel smoothing size) -1 
                         if (polar_basis == 'refregier'):
                             arr_res = \
                                     p_shapelet.polar_shapelets_refregier(n,m,sigma)(R,Phi).flatten() 
@@ -402,14 +402,14 @@ def shapelet_decomposition(N1=20,N2=20, basis = 'XY', solver = 'sparse', noise =
                         else: 
                             #base_coefs_r[n,m] = coef_r/np.sqrt(arr_norm2)
                             #base_coefs_im[n,m] = coef_im/np.sqrt(arr_norm_im2)
-                            base_coefs[n,m] = (coef_res/np.sqrt(arr_norm2_res)).real
+                            base_coefs[n,m] = coef_res/np.sqrt(arr_norm2_res)
                             shapelet_reconst = shapelet_reconst \
-                                    + (coef_res*arr_res/arr_norm2_res).real
+                                    + coef_res*arr_res/arr_norm2_res
                     else: break
         elif(basis == 'XY'):
              for k in xrange(N1*N2):
                 m,n = k/N1, k%N1 
-                if (m+n <= (78/sigma - 1)): 
+                if (m+n <= n_max): 
                     arr = shapelet2d(m,n,x0=x0,y0=y0,sx=sigma,sy=sigma)(X,Y).flatten()
                     #arr2 = shapelet2d(m,n,x0=x0,y0=y0,sx=0.5*sigma,sy=0.5*sigma)(X,Y).flatten()
                     #arr3 = shapelet2d(m,n,x0=x0,y0=y0,sx=1.5*sigma,sy=2.*sigma)(X,Y).flatten()
@@ -482,7 +482,7 @@ def shapelet_decomposition(N1=20,N2=20, basis = 'XY', solver = 'sparse', noise =
             
             coeffs_lasso, reconstruction_lasso, residual_lasso, \
             residual_energy_fraction_lasso, recovered_energy_fraction_lasso, \
-            n_nonzero_coefs_lasso = solver_lasso_reg(D, signal, 0)
+            n_nonzero_coefs_lasso = solver_lasso_reg(D, signal, 0.0000001)
             
             plot_solution(N1,N2,cube_real, img_idx, reconstruction_lasso.real, residual_lasso.real, \
                     coeffs_lasso, recovered_energy_fraction_lasso, residual_energy_fraction_lasso, \

codes/code/python_codes/trial_codes/polar_shapelet_decomp.py
@@ -14,85 +14,85 @@ from scipy.integrate import quad,dblquad
 Pi = math.pi
 factorial = math.factorial
 
-def coeff_refregier(N, M, beta):
-    
-    """ Calculate coefficients for refregier
-    
-    @param N Energy quantum number
-    @param M Angular quantum number
-    @param beta Shapelet scale factor
+def coeff(N, M, beta):
+    """ 
+    Return normalization coefficients for refregier shapelets
 
-    @return B, C Normalization coefficients for shapelets
     """
-
-    B = (-1)**((N - np.abs(M))/2) / beta**(np.abs(M) + 1)
-    C = (float(factorial(int((N-np.abs(M))/2)))/(Pi*float(factorial((N+np.abs(M))/2))))**(.5)
-    return B, C
+    A = (-1)**((N - np.abs(M))/2) / beta**(np.abs(M) + 1)
+    B = (2*float(factorial(int((N-np.abs(M))/2)))/(float(factorial((N+np.abs(M))/2))))**(.5)
+    C = .5*np.sqrt(np.abs(M)/Pi)
+    return A,B, C
 
 def polar_shapelets_refregier(N, M, beta):
 
-    """ Return callable function for polar shapelets expression from refregier
-
-    @param N, M Energy and momentum quantum numbers respectively
-    @param beta Shapelet scale
-
-    @return Callable function for shapelets in polar coordinates 
     """
-    coeff_1, coeff_2 = coeff_refregier(N, M, beta)
+    Return callable function for normalized generalized Laguerre polynomials with separated 
+    exp(-1j*M*phi) to Cos (M>0) and Sin (M<0) and ordinary radial part for M == 0
+    """
+    coeff_1, coeff_2, coeff_3 = coeff(N, M, beta)
     gen_laguerre = special.genlaguerre(n=(N-np.abs(M))/2, alpha=np.abs(M))
-
-    laguer_N_M = lambda x, phi: coeff_1 * coeff_2 * x**(np.abs(M)) * gen_laguerre(x**2/beta**2) * np.exp(-x**2/2./beta**2) * np.exp(-1j*M*phi)
     
+    if (M > 0):
+        laguer_N_M = lambda x, phi: coeff_1 * coeff_2 * x**(np.abs(M)) * gen_laguerre(x**2/beta**2) * np.exp(-x**2/2./beta**2) * coeff_3 * np.cos(M*phi) 
+    elif (M < 0):
+        laguer_N_M = lambda x, phi: coeff_1 * coeff_2 * x**(np.abs(M)) * gen_laguerre(x**2/beta**2) * np.exp(-x**2/2./beta**2) * coeff_3 * np.sin(M*phi)
+    elif (M == 0):
+        laguer_N_M = lambda x, phi: coeff_1 * coeff_2 * x**(np.abs(M)) * gen_laguerre(x**2/beta**2) * np.exp(-x**2/2./beta**2)
     return laguer_N_M
 
 
+
+"""
+Trial function for testing
+BEGIN
+"""
 def polar_shapelets_real(N, M, beta):
 
-    coeff_1, coeff_2 = coeff_refregier(N, M, beta)
+    coeff_1, coeff_2, coeff_3 = coeff(N, M, beta)
     gen_laguerre = special.genlaguerre(n=(N-np.abs(M))/2, alpha=np.abs(M))
 
-    laguer_N_M = lambda x, phi: coeff_1 * coeff_2 * x**(np.abs(M)) * gen_laguerre(x**2/beta**2) * np.exp(-x**2/2./beta**2) * np.exp(-1j*M*phi).real
-    
+    laguer_N_M = lambda x: coeff_1 *coeff_2 * x**(np.abs(M)) * gen_laguerre(x**2/beta**2) * np.exp(-x**2/(2*beta**2))
     return laguer_N_M
 
 def polar_shapelets_imag(N, M, beta):
     
-    coeff_1, coeff_2 = coeff_refregier(N,M,beta)
+    coeff_1, coeff_2,coeff_3 = coeff(N,M,beta)
 
-    gen_laguerre = special.genlaguerre(n = (N-np.abs(M))/2, alpha = np.abs(M))
+    laguer_N_M = lambda x: 0
 
-    laguer_N_M = lambda x, phi: coeff_1 * coeff_2 * x**(np.abs(M)) * gen_laguerre(x**2/beta**2) * np.exp(-x**2/2./beta**2) * np.exp(-1j*M*phi).imag
-    
+    if (M>0):
+        laguer_N_M = lambda x: coeff_3 * np.cos(M*x)
+    elif (M<0):
+        laguer_N_M = lambda x: coeff_3 * np.sin(M*x)
+        
     return laguer_N_M
+
+"""
+Trial functions for testing
+END
+"""
 
 #From Berry et al. Eq (12)
 
 def coeff_berry(N,M,beta):
-    
-    """ Calculate coefficients for expression from berry
-
-    @param N,M Energy and momentum quantum numbers respectively
-    @param beta Shapelet scale
-
-    @return Normalization coefficient as in berry
-    """
-    return (2.*float(factorial((N-np.abs(M)/2)))/(Pi*beta**2*float(factorial((N+np.abs(M))/2))))**(0.5)
+    return (2.*float(factorial((N-np.abs(M)/2)))/(beta**2*float(factorial((N+np.abs(M))/2))))**(0.5)
 
 def polar_shapelets_berry(N,M, beta):
 
-    """ Callable function 
-
-    """
     coeff = coeff_berry(N,M,beta)
     gen_laguerre = special.genlaguerre(n = (N-np.abs(M))/2, alpha = np.abs(M))
 
-    laguer_N_M = lambda r, phi: coeff * (r**2/beta**2)**(np.abs(M)/2) * np.exp(-(r**2/beta**2)/2.) * gen_laguerre(r**2/beta**2) * np.exp(-1j*M*phi)
+    laguer_N_M = lambda r, phi: coeff * (r/beta)**(np.abs(M)) * np.exp(-(r**2/beta**2)/2.) * gen_laguerre(r**2/beta**2) * np.exp(-1j*M*phi)
     
     return laguer_N_M
 
 
 def plot_shapelets(N,M, beta):
-    
+
+    """
+    Make a 2D grid for visualizing the shapelets
+    """
     X = np.linspace(-5, 5, 100)
     Y = np.linspace(-5, 5, 100)
     Xv, Yv = meshgrid(X,Y)
@@ -102,43 +102,27 @@ def plot_shapelets(N,M, beta):
         for j in xrange(np.shape(Xv)[1]):
             phi[i,j] = math.atan2(Yv[i,j], Xv[i,j])
 
-    func = polar_shapelets_berry(N, M, beta)(R, phi).real
+    func = polar_shapelets_refregier(N, M, beta)(R, phi)
     im = imshow(func, cmap=cm.bwr)
     plt.show()
-
-def check_orthonormality():
-    
-    X = np.linspace(-5, 5, 100)
-    Y = np.linspace(-5, 5, 100)
-    Xv, Yv = meshgrid(X,Y)
-    R = np.sqrt(Xv**2 +  Yv**2)
-    phi = np.zeros_like(R)
-    for i in xrange(np.shape(Xv)[0]):
-        for j in xrange(np.shape(Xv)[1]):
-            phi[i,j] = math.atan2(Yv[i,j], Xv[i,j])
-    
-    print(\
-            np.abs(\
-            np.dot(\
-            polar_shapelets_refregier(2,2,1)(R,phi).flatten(),\
-            polar_shapelets_refregier(2,2,1)(R,phi).flatten())), \
-            np.abs(\
-            np.dot(\
-            polar_shapelets_berry(2,2,1)(R,phi).flatten(), \
-            polar_shapelets_berry(2,2,1)(R,phi).flatten()) ))
 
 if __name__ == "__main__":
     
   
-    #plot_shapelets(0,0, 1)
-    n =1; m = 1; beta = 1.;
+    check = 0
+    plot_shapelets(4,2, 1)
     
-    for n in xrange(10):
-        for m in xrange(-n,n+1,2):
-            print('berry')
-            print(dblquad(lambda r,phi: r*polar_shapelets_berry(n,m,beta)(r,phi).conjugate()*polar_shapelets_berry(n,m,beta)(r,phi), 0, np.inf, lambda r: 0, lambda r: 2*Pi))
-            print('refregier')
-            print(dblquad(lambda r,phi: r*polar_shapelets_refregier(n,m,beta)(r,phi).conjugate()*polar_shapelets_refregier(n,m,beta)(r,phi), 0, np.inf, lambda r: 0, lambda r: 2*Pi))    
-    #check_orthonormality() 
+
+    if (check == 1):
+        for n in xrange(10):
+            for m in xrange(-n,n+1,2):
+                print(n,m)
+                #print('berry')
+                #print(dblquad(lambda r,phi: r*polar_shapelets_berry(n,m,beta)(r,phi).conjugate()*polar_shapelets_berry(n,m,beta)(r,phi), 0, np.inf, lambda r: 0, lambda r: 2*Pi))
+                print('refregier')
+                print('radial_part')
+                print(quad(lambda r: r*polar_shapelets_real(n,m,beta)(r)**2, 0, np.inf))
+                print('angular_part')
+                print(quad(lambda phi: polar_shapelets_imag(n,m,beta)(phi)**2,0, 2*Pi))
     #print(polar_shapelets(1,1,0,Pi/2, 1.))     
    