bda0959bac34f8abdafb896ec5123305779d900b
Andrija Kostic <kole11koska@gmail.com>
2017-07-03 18:45:03 +0200

    update of trial_codes

--- codes/code/python_codes/trial_codes/polar_shapelet_decomp.py
+++ codes/code/python_codes/trial_codes/polar_shapelet_decomp.py
@@ -63,19 +63,20 @@ def polar_shapelets_refregier(N, M, beta):
 Trial function for testing
 BEGIN
 """
-def polar_shapelets_real(N, M, beta):
+def polar_shapelets_radial(N, M, beta):
 
     coeff_1, coeff_2, coeff_3 = coeff(N, M, beta)
     gen_laguerre = special.genlaguerre(n=(N-np.abs(M))/2, alpha=np.abs(M))
 
-    laguer_N_M = lambda x: coeff_1 *coeff_2 * x**(np.abs(M)) * gen_laguerre(x**2/beta**2) * np.exp(-x**2/(2*beta**2))
+    laguer_N_M = lambda x: coeff_1 *coeff_2 * x**(np.abs(M)) \
+            * gen_laguerre(x**2/beta**2) * np.exp(-x**2/(2*beta**2))
     return laguer_N_M
 
-def polar_shapelets_imag(N, M, beta):
+def polar_shapelets_angular(N, M, beta):
     
     coeff_1, coeff_2,coeff_3 = coeff(N,M,beta)
 
-    laguer_N_M = lambda x: 0
+    laguer_N_M = lambda x: 1
 
     if (M>0):
         laguer_N_M = lambda x: coeff_3 * np.cos(M*x)
@@ -149,11 +150,11 @@ def plot_shapelets_test_image(N1,M1,N2,M2,N3,M3, beta):
         for j in xrange(np.shape(Xv)[1]):
             phi[i,j] = math.atan2(Yv[i,j], Xv[i,j])
     
-    a = 0.1
+    a = 0
     b = 0
-    c = 0
+    c = 1
 
-    func = a* polar_shapelets_refregier(N1,M1,beta)(R,phi) + b*polar_shapelets_refregier(N2, M2, beta)(R, phi) + c*polar_shapelets_refregier(N3,M3,beta)(R,phi)
+    func = a*polar_shapelets_refregier(N1,M1,beta)(R,phi) + b*polar_shapelets_refregier(N2, M2, beta)(R, phi) + c*polar_shapelets_refregier(N3,M3,beta)(R,phi)
     im = imshow(func, cmap=cm.bwr)
     
     # Save the Ns and Ms of shapelets for the test image, and the appropriate coeffs
@@ -180,19 +181,36 @@ def plot_shapelets_test_image(N1,M1,N2,M2,N3,M3, beta):
 if __name__ == "__main__":
     
   
-    check = 0; beta = 1.
-    
-    plot_shapelets_test_image(0,0, 1,0, 0,0 , 1.2)
+    check = -1; beta = 1.; check_dep = 1;
+
+    #plot_shapelets_test_image(0,0, 1,1, 1,-1, 1.2)
     
 
     if (check == 1):
         for n in xrange(10):
-            for m in xrange(0,n,1):
+            for m in xrange(-n,n+1,2):
                 print(n,m)
                 #print('berry')
                 #print(dblquad(lambda r,phi: r*polar_shapelets_berry(n,m,beta)(r,phi).conjugate()*polar_shapelets_berry(n,m,beta)(r,phi), 0, np.inf, lambda r: 0, lambda r: 2*Pi))
-                print('nakajima')
-                print('radial_part')
-                print(quad(lambda r: r*polar_shapelets_nakajima(n,m,beta)(r,0)**2, 0, np.inf)) 
-    #print(polar_shapelets(1,1,0,Pi/2, 1.))     
-   
+                print('refregier')
+                print(quad(lambda x: polar_shapelets_angular(n,m,beta)(x)**2, 0,2*Pi)) 
+    #print(polar_shapelets(1,1,0,Pi/2, 1.))
+    
+    ## Test linear dependance
+    if check_dep == 1:
+        X = np.linspace(-5, 5, 78)
+        Y = np.linspace(-5, 5, 78)
+        Xv, Yv = meshgrid(X,Y)
+        R = np.sqrt(Xv**2 +  Yv**2)
+        phi = np.zeros_like(R)
+        for i in xrange(np.shape(Xv)[0]):
+            for j in xrange(np.shape(Xv)[1]):
+                phi[i,j] = math.atan2(Yv[i,j], Xv[i,j])
+        for n1 in xrange(10):
+            for m1 in xrange(-n1,n1+1,2):
+                for n2 in xrange(10):
+                    for m2 in xrange(-n2,n2+1,2):
+                        dot_prod = np.dot(polar_shapelets_refregier(n1,m1,beta)(R,phi).flatten(), polar_shapelets_refregier(n2,m2,beta)(R,phi).flatten())
+                        if dot_prod > 0.01:
+                            print n1,m1, ' dot ', n2,m2, ':'
+                            print dot_prod
